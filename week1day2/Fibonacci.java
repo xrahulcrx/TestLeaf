776b9227be82f6d37e88b7ee3e75e81dccde78e3
xrahulcrx <rahulcr2797@gmail.com>
2025-10-13 19:22:56 +0530

    Create Fibonacci.java

---
+++ week1day2/Fibonacci.java
@@ -0,0 +1,36 @@
+public class Fibonacci {
+
+	public static int FibonacciSeries(int n) {
+		// TODO Auto-generated method stub
+
+		if (n <= 1)
+			return n;
+
+		int a = 0;
+		int b = 1;
+
+		for (int i = 0; i < n; i++) {
+			
+
+			int sum = a + b;
+			a = b;
+			b = sum;
+
+		}
+
+		return b;
+
+	}
+
+	public static void main(String[] args) {
+		Fibonacci fs = new Fibonacci();
+		
+		int n = 8;
+
+		System.out.println("Fibonacci series up to "+n+"th term:");
+		for (int i = 0; i <= n; i++) {
+			System.out.print(fs.FibonacciSeries(i) + " ");
+		}
+	}
+
+}
