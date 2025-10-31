2f585621fa5e1bd0cb55ac347283b2118e79a685
xrahulcrx <rahulcr2797@gmail.com>
2025-10-31 13:23:38 +0530

    Update FacebookLogin.java

--- week2day1/FacebookLogin.java
+++ week2day1/FacebookLogin.java
@@ -5,22 +5,23 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class FacebookLogin {
 
 	public static void main(String[] args) throws InterruptedException {
-
+		// TODO Auto-generated method stub
+		
 		
 		ChromeOptions options = new  ChromeOptions();
 		options.addArguments("--guest");
 
 		
 		ChromeDriver driver = new ChromeDriver(options);
-		//get url
+		
 		driver.get("https:www.facebook.com");
-    //maximize
 		driver.manage().window().maximize();
-
-    //dummy variables
+		  
 		driver.findElement(By.id("email")).sendKeys("testleaf.2023@gmail.com");
 		driver.findElement(By.id("pass")).sendKeys("Tuna@321");
 		driver.findElement(By.name("login")).click();
+		
+		System.out.println(driver.getTitle());
 		Thread.sleep(5000);
 		driver.close();
 
