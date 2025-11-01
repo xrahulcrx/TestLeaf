c328b2382fc197b14249eff3c4543e5a26a21312
xrahulcrx <rahulcr2797@gmail.com>
2025-11-01 18:29:51 +0530

    Create RemoveDupeString.java

---
+++ week3day1/RemoveDupeString.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class RemoveDupeString {
+	
+	public static void main(String[] args) {
+		String text = "We learn Java basics as part of java sessions in java week1";
+		
+		String[] words = text.split(" ");
+		
+		int count = 0;
+		
+		for(int i = 0; i < words.length; i++) {
+			for(int j = i +1; j < words.length ; j++) {
+				
+				if(words[i].equalsIgnoreCase(words[j])) {
+					words[j] = " ";
+					count++;
+				}
+				
+			}			
+			
+
+		}
+		
+		if(count > 1) {
+			for(String s:words) {
+				System.out.print(s+" ");
+			}
+		}
+	}
+}
