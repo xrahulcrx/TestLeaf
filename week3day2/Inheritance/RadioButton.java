23c5407288d42e6afafe14e92d19e83bd624db87
xrahulcrx <rahulcr2797@gmail.com>
2025-11-05 17:40:28 +0530

    Create RadioButton.java

---
+++ week3day2/Inheritance/RadioButton.java
@@ -0,0 +1,11 @@
+package week3day2;
+
+public class RadioButton  extends Button{
+	
+	public void selectRadioButton() {
+		
+		System.out.println("Method from RadioButton extends Button");
+		
+	}
+
+}
