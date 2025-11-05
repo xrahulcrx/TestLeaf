cca124d3730201e2f5e74db7ba513b35f87b9b64
xrahulcrx <rahulcr2797@gmail.com>
2025-11-05 17:38:59 +0530

    Create WebElement.java

---
+++ week3day2/Inheritance/WebElement.java
@@ -0,0 +1,17 @@
+package week3day2;
+
+public class WebElement {
+	
+	public void click() {
+		
+		System.out.println("Base class Webelement click");
+		
+	}
+	
+	public void setText(String text) {
+		
+		System.out.println("setText method from  Weblement "+ text);
+		
+	}
+
+}
