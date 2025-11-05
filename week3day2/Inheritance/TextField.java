7b9562f04dcb663d66e101b188978b535d08b356
xrahulcrx <rahulcr2797@gmail.com>
2025-11-05 17:39:45 +0530

    Create TextField.java

---
+++ week3day2/Inheritance/TextField.java
@@ -0,0 +1,11 @@
+package week3day2;
+
+public class TextField extends WebElement {
+	
+	public void getText() {
+		
+		System.out.println("Getting getText method from Textfield extends Weblement");
+		
+	}
+
+}
