8e512d3400cdf3d159f82082e427b11c5693cc31
xrahulcrx <rahulcr2797@gmail.com>
2025-11-05 18:19:02 +0530

    Create BasePage.java

---
+++ week3day2/Override/BasePage.java
@@ -0,0 +1,22 @@
+package week3day2_Poly;
+
+public class BasePage {
+	
+	public void findElement() {
+		System.out.println("Base class findElement");
+	}
+	
+	public void clickElement() {
+		System.out.println("Base class clickElement");
+	}
+	
+	public void enterText() {
+		System.out.println("Base class enterText");
+	}
+	
+	public void performCommonTasks() {
+		System.out.println("Base class performCommonTasks");
+	}
+	
+
+}
