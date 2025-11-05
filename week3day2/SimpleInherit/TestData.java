a03b7f87959ebe388b24a07f2ac806a7d1cef30a
xrahulcrx <rahulcr2797@gmail.com>
2025-11-05 19:13:54 +0530

    Create TestData.java

---
+++ week3day2/SimpleInherit/TestData.java
@@ -0,0 +1,19 @@
+package week3day2Simple;
+
+public class TestData {
+	
+	public void enterCredentials() {
+		
+		System.out.println("From TestData enterCredentials");
+		
+	}
+	
+	public void navigateToHomePage() {
+		
+		System.out.println("From TestData navigateToHomePage");
+		
+	}
+	
+	
+
+}
