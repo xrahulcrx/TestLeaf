f859fd12c699e105f6e44054dbd0541fcd2a6c7c
xrahulcrx <rahulcr2797@gmail.com>
2025-11-05 17:39:23 +0530

    Create Button.java

---
+++ week3day2/Inheritance/Button.java
@@ -0,0 +1,15 @@
+package week3day2;
+
+public class Button  extends WebElement{
+	
+	
+	public void submit() {
+		
+		System.out.println("Button Class method - submit extends WebElement");
+			
+		
+	}
+	
+	
+
+}
