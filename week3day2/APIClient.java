7ae1087fd2d114c47d8c96a39f80216b25ed2b8f
xrahulcrx <rahulcr2797@gmail.com>
2025-11-05 19:22:03 +0530

    Update APIClient.java

--- week3day2/APIClient.java
+++ week3day2/APIClient.java
@@ -7,7 +7,7 @@ public class APIClient {
 	public void sendRequest(String endpoint, String requestBody, boolean requestStatus) {
 		
 		if(requestStatus) {
-			System.out.println("The request status is true, so endpoint text : "+endpoint+" and requestBody text : "+requestBody);
+			System.out.println("The request status is true,  endpoint : "+endpoint+" and requestBody : "+requestBody);
 		}else {
 			System.out.println("The request status is false");
 		}
@@ -18,8 +18,8 @@ public class APIClient {
 		APIClient apic = new APIClient();
 		
 		apic.sendRequest("Hellow World");
-		apic.sendRequest("The start", "The end", true);
-		apic.sendRequest("The end", "The start", false);
+		apic.sendRequest("192.168.1.10", "plain text", true);
+		apic.sendRequest("192.168.1.1", "JSON", false);
 	}
 
 }
