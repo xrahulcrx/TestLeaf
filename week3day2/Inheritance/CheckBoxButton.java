3cbc12db79a4b401a03e92e2773a9527c5a39268
xrahulcrx <rahulcr2797@gmail.com>
2025-11-05 17:40:08 +0530

    Create CheckBoxButton.java

---
+++ week3day2/Inheritance/CheckBoxButton.java
@@ -0,0 +1,12 @@
+package week3day2;
+
+public class CheckBoxButton extends Button {
+	
+	public void clickCheckButton() {
+		
+		System.out.println("Method from CheckBoxButton - clickCheckButton");
+		
+		
+	}
+
+}
