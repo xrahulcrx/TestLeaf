819c161acfd4d72d6a65b04937257a26b23b122a
xrahulcrx <rahulcr2797@gmail.com>
2025-11-11 22:33:06 +0530

    Update MyntraList.java

--- week4day2/MyntraList.java
+++ week4day2/MyntraList.java
@@ -1,7 +1,6 @@
 package week4day1HA;
 
 import java.time.Duration;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -17,49 +16,51 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class MyntraList {
 	
 	public static void main(String[] args) throws InterruptedException {
-		
+
+		//chrome options initialize
 		ChromeOptions options = new ChromeOptions();
-		
 		options.addArguments("--guest");
-		
+
+		//driver
 		ChromeDriver driver  = new ChromeDriver(options);
-		
-		
 		driver.manage().window().maximize();
 		
 		
 		driver.get("https://www.myntra.com/");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		
+		//search
 		driver.findElement(By.xpath("//input[@class='desktop-searchBar']")).sendKeys("bags" + Keys.ENTER);
 		
-		
+		//passing and selecting arguments
 		passLocate(driver, By.xpath("//label[contains(@class,'gender-label') and normalize-space(.)='Men']"));
 		Thread.sleep(1000);
 		passLocate(driver, By.xpath("//label[contains(@class, 'customCheckbox') and contains(normalize-space(.), 'Laptop Bag')]"));
 		Thread.sleep(1000);
-		
+
+		//list to get webelements
 		List<WebElement> brandNames = driver.findElements(By.xpath("//h3[@class='product-brand']"));
-		
 		List<WebElement> productTitle = driver.findElements(By.xpath("//h4[@class='product-product']"));
-		
+
+		//set for unique
 		Set<String> brandNamesUnique = new HashSet<>();
 		Set<String> productTitleUnique = new HashSet<>();
-		
+
+		//passing brand names to set
 		for (WebElement each : brandNames) {
 			
 			brandNamesUnique.add(each.getText());
 			
 		}
-		
+
+		//passing unique titles to set
 		for (WebElement each : productTitle) {
 			
 			productTitleUnique.add(each.getText());
 			
 		}
 		
-		
-		System.out.println("Brand Count with Duplicates :" +brandNames.size()+ " and Brand Count with Duplicates :" +brandNamesUnique.size());
+		//printing brand names
+		System.out.println("Brand Count with Duplicates :" +brandNames.size()+ " and Brand Count without Duplicates :" +brandNamesUnique.size());
 		System.out.println("List of Brand Names:");
 		
 		
@@ -69,29 +70,18 @@ public class MyntraList {
 			
 		}
 		
-		
-		System.out.println("\nProduct Titles Count with Duplicates :" +productTitle.size()+ " and Product Titles Count with Duplicates :" +productTitleUnique.size());
+		//printing titles
+		System.out.println("\nProduct Titles Count with Duplicates :" +productTitle.size()+ " and Product Titles Count without Duplicates :" +productTitleUnique.size());
 		System.out.println("List of Product Titles: ");
 		
 		for (String title : productTitleUnique) {
 			System.out.println(title);
 			
 		}
-		
-		
-		
-
-		
-		
-		
-		
-		
-		
-		
+			
 	}
 	
-	
-	
+	//argument locator passing menthod	
 	public static void passLocate(ChromeDriver driver, By locator) {
 	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
